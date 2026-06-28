d86e0c0d1ead35b73d8867bb24c0462723b360a3
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-06-28 16:15:52 -0500

    commit - Principal .

--- src/conexion/ConexionMySQL.java
+++ src/conexion/ConexionMySQL.java
@@ -1,4 +1,23 @@
 package conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConexionMySQL {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/secuencia_db";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "Tapiero123";
+
+    public static Connection conectar() {
+
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }   // clase conexión!
 }
