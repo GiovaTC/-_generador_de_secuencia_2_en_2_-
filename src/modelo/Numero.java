056b00c274ba9994c0f6716ad3d4bc86f268dffb
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-06-28 16:18:23 -0500

    commit - Numero / .

--- src/modelo/Numero.java
+++ src/modelo/Numero.java
@@ -1,4 +1,31 @@
 package modelo;
 
 public class Numero {
+
+    private int id;
+    private int numero;
+
+    public Numero() {
+
+    }
+
+    public Numero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 }
