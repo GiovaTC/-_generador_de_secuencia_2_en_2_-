cd034282326d1642986c88064417a8d8c52bb9ea
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-06-28 16:30:29 -0500

    commit - Principal .

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Principal {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import dao.NumeroDAO;
+import modelo.Numero;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Principal {
+
+    public static void main(String[] args) {
+
+        NumeroDAO dao = new NumeroDAO();
+
+        System.out.println("Generando secuencia...");
+
+        for(int i = 2; i <= 100; i += 2){
+
+            Numero numero = new Numero(i);
+
+            dao.guardar(numero);
+
+            System.out.println("Guardado: " + i);
+
         }
-    }
+        System.out.println();
+        dao.mostrarTodos();
+
+    }   
 }
